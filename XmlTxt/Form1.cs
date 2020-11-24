8559a294b80836bd1d2789c7fc7ce72919aad4ad
Junior Baratella <molusko.rp@hotmail.com>
2020-11-24 15:09:36 -0300

    Starting the Project

---
+++ XmlTxt/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XmlTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = @"E:\Clientes\Romulus\Loja1\NfeEntrada_Loja1\202005\Xml\";
+            DirectoryInfo directory = new DirectoryInfo(path);
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                if(file.Extension.ToUpper() == ".XML")
+                {
+
+                    continue;
+                }
+                file.Delete();
+            }
+        }
+    }
+}
